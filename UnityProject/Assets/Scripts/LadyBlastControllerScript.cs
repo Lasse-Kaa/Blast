02df4c70048b7ac999b41dd248527bfcc910e2b7
Lasse-Kaa <lassekiil@hotmail.com>
2014-04-25 20:24:48 +0200

    Jumping
    -The PC now has animation when move to either side
    -Added script, so PC now jumps on 'Space'

--- UnityProject/Assets/Scripts/LadyBlastControllerScript.cs
+++ UnityProject/Assets/Scripts/LadyBlastControllerScript.cs
@@ -5,15 +5,39 @@ public class LadyBlastControllerScript : MonoBehaviour {
 	
 	public float maxSpeed = 10f;
 	bool facingRight = true;
+
+	// Ground state: Will check whether or not the PC is standing on an object
+	// and control the fall animation
+	bool onGround = false;
+	public Transform groundCheck;
+	float groundRadius = 2.0f;
+	public LayerMask whatIsGround;
+	public float jumpForce = 700f;
+
+	// Reference to animator
+	Animator anim;
 	
 	// Use this for initialization
 	void Start () {
-		
+
+		// Gets component from animator
+		anim = GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
+
+		// Will check whether or not the PC is touching anything. 
+		// If result is true, PC is on the ground
+		onGround = Physics2D.OverlapCircle (groundCheck.position, groundRadius, whatIsGround);
+		anim.SetBool ("Ground", onGround);
+
+		anim.SetFloat ("vSpeed", rigidbody2D.velocity.y);
+
 		float move = Input.GetAxis ("Horizontal");
+
+		// Sets parameter Speed to absolute value move
+		anim.SetFloat ("Speed", Mathf.Abs (move));
 		
 		rigidbody2D.velocity = new Vector2(move * maxSpeed, rigidbody2D.velocity.y);
 		
@@ -24,7 +48,16 @@ public class LadyBlastControllerScript : MonoBehaviour {
 		else if(move > 0 && facingRight)
 			Flip ();
 	}
-	
+
+	// The jump input is listened for inside Update instead of 
+	// Fixed Update to make it more accurate. Otherwise the input might be missed
+	void Update(){
+		if(onGround && Input.GetButtonDown("Jump")){
+			anim.SetBool("Ground", false);
+			rigidbody2D.AddForce(new Vector2(0, jumpForce));
+		}
+	}
+
 	// Flip takes the world and flips the world 
 	// and the animation to save animation
 	void Flip(){
---
+++ UnityProject/Assets/Scripts/ScrollScript.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScrollScript : MonoBehaviour {
+
+	public float speed = 0f;
+	
+	// Update is called once per frame
+	void Update () {
+		renderer.material.mainTextureOffset = new Vector2((Time.time * speed)%1,1.0f);
+	}
+}
